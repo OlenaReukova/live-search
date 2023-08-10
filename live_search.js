44001fd68fe86b0264645a5f90571ba0fc99bfc8
Olena Reukova <kronverk01@gmail.com>
2023-08-10 08:54:54 +0100

    adding Mark and improved to inline hiding code instead of using class HIDE

--- live_search.js
+++ live_search.js
@@ -1,17 +1,28 @@
-document.querySelector('#elastic').oninput = function () {
-  let val = this.value.trim();
+document.querySelector('#elastic').addEventListener('input', function () {
+  let currentValue = this.value.trim().toLowerCase();
   let elasticItems = document.querySelectorAll('.elastic li');
-  if (val != '') {
-    elasticItems.forEach(function (elem) {
-      if (elem.innerText.search(val) == -1) {
-        elem.classList.add('hide');
-      } else {
-        elem.classList.remove('hide');
-      }
-    });
-  } else {
-    elasticItems.forEach(function (elem) {
-      elem.classList.remove('hide');
-    });
-  }
-};
+
+  elasticItems.forEach(function (elem) {
+    if (elem.textContent.toLowerCase().search(currentValue) === -1) {
+      elem.style.display = 'none';
+    } else {
+      elem.style.display = null;
+      let str = elem.textContent;
+      elem.innerHTML = insertMark(
+        str,
+        str.toLowerCase().indexOf(currentValue),
+        currentValue.length
+      );
+    }
+  });
+});
+
+function insertMark(string, position, length) {
+  return (
+    string.slice(0, position) +
+    '<mark>' +
+    string.slice(position, position + length) +
+    '</mark>' +
+    string.slice(position + length)
+  );
+}
